197f8661a9f301e83cf5f7607ebaf3b231311901
kevin <willardquing@gmail.com>
2019-10-23 15:46:05 +0800

    Made all test work somehow

--- src/app/beeper-settings/beeper-settings.component.spec.ts
+++ src/app/beeper-settings/beeper-settings.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BeeperSettingsComponent } from './beeper-settings.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormsModule } from '@angular/forms';
+import { AppModule } from '../app.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('BeeperSettingsComponent', () => {
   let component: BeeperSettingsComponent;
   let fixture: ComponentFixture<BeeperSettingsComponent>;
+  // const fb = new FormBuilder();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BeeperSettingsComponent ]
+      declarations: [ BeeperSettingsComponent ],
+      imports: [
+        BrowserModule,
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        // { provide: FormBuilder, useValue: fb }
+      ]
     })
     .compileComponents();
   }));
