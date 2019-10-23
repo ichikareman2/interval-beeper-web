0a9f0f3c27e83f47b38b98d984ee09b6e4992c89
kevin <willardquing@gmail.com>
2019-10-23 16:21:48 +0800

    change e2e for ci test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('interval-beeper-web app is running!');
+    expect(page.getTitleText()).toEqual(Promise.resolve`Interval Beeper`);
   });
 
   afterEach(async () => {
