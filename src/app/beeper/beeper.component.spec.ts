0496b807a44427eecdb891e7186d080540022500
kevin <willardquing@gmail.com>
2019-10-26 21:28:00 +0800

    Just update dependencies

--- src/app/beeper/beeper.component.spec.ts
+++ src/app/beeper/beeper.component.spec.ts
@@ -1,10 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { cold, getTestScheduler } from 'jasmine-marbles';
 import { BeeperComponent } from './beeper.component';
 
 describe('BeeperComponent', () => {
   let component: BeeperComponent;
   let fixture: ComponentFixture<BeeperComponent>;
+  // let mockIntervalBeeperService: { state$}
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
